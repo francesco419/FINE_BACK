431afc853b179c468cd1292cb57c117d90848f01
Lee Sang Hyeon <francesco419@naver.com>
2023-09-30 01:05:42 +0900

    test 2

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.use(
     credentails: true
   })
 );
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(
   cors({
     origin: 'http://localhost:3000',
