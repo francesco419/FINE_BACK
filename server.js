da1fc720fa6482474e4327376809b84af31a643f
Lee Sang Hyeon <francesco419@naver.com>
2023-09-30 01:12:23 +0900

    test 2

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://finedition.kr/');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
