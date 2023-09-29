c597e24fe725ad7d2b991ec35c5cfad58f931957
Lee Sang Hyeon <francesco419@naver.com>
2023-09-30 04:04:34 +0900

    test 11

--- action.js
+++ action.js
@@ -1,5 +1,9 @@
 const multer = require('multer');
 const getConnection = require('./config/getConnection');
+var mysql = require('mysql');
+require('dotenv').config();
+//const dbconfig = require('./dbconfig.json');
+const { POOL_PASSWORD, POOL_HOST, POOL_USER, POOL_DATABASE } = process.env;
 
 exports.test = (req, res) => {
   console.log('CONNECTION SUCCESS');
@@ -54,7 +58,7 @@ exports.postRegister = (req, res) => {
   });
 };
 
-exports.getInfoData = (req, res) => {
+/* exports.getInfoData = (req, res) => {
   console.log(req.query.id);
   getConnection.getConnection(function (err, conn) {
     const exec = conn.query(
@@ -72,4 +76,39 @@ exports.getInfoData = (req, res) => {
       }
     );
   });
+}; */
+
+exports.getInfoData = (req, res) => {
+  var connection = mysql.createConnection({
+    host: POOL_HOST,
+    user: POOL_USER,
+    password: POOL_PASSWORD,
+    port: POOL_DATABASE
+  });
+
+  connection.connect(function (err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+    console.log('Connected to database.');
+  });
+
+  connection.end();
+  /* getConnection.getConnection(function (err, conn) {
+    const exec = conn.query(
+      `select * from info where contentid=(?);`,
+      [req.query.id],
+      (err, result) => {
+        conn.release();
+        if (err) {
+          console.log('info failed');
+          return res.send({ result: result, flag: false });
+        } else {
+          console.log('info success');
+          return res.send({ result: result, flag: true });
+        }
+      }
+    );
+  }); */
 };
