15382d5e4f53f1f1f2f55851a6e946a0731413fb
Lee Sang Hyeon <francesco419@naver.com>
2023-10-16 03:38:20 +0900

    test 1016

--- server.js
+++ server.js
@@ -98,7 +98,6 @@ app.post('/testimage', upload.array('file'), async (req, res) => {
 app.post('/test', actionApi.test);
 app.post('/logincheck', actionApi.postLoginCheck);
 app.post('/register', actionApi.postRegister);
-//app.post('/register', actionApi.postRegister);
 
 app.post('/getinfo', actionApi.getInfoData);
 
